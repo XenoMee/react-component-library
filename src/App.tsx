a6d42d03b597583e0ebf61a739267cfcabe8e5bc
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-29 15:59:50 +0300

    add container class to main element

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import BadgesBlock from './Components/BadgesBlock';
 
 const App = () => {
   return (
-    <main className='grid gap-20 justify-center'>
+    <main className='container grid gap-20 justify-center'>
       <h1 className='h1'>React Component Library</h1>
       <div className='components'>
         <BadgesBlock />
