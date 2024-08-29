96550bf096f857fe3518e75114ca4308a24d276a
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-29 13:31:21 +0300

    create Badge interface type

--- src/Components/Badge.tsx
+++ src/Components/Badge.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Badge = ({ children, shape, variant }) => {
+interface Badge {
+  children: React.ReactNode;
+  shape?: 'pill' | 'square';
+  variant?: string;
+}
+
+const Badge = ({ children, shape, variant }: Badge) => {
   const badgeClasses = classNames({
     'rounded-full': shape === 'pill',
     'rounded-sm': shape === 'square',
     [`bg-${variant}-200 text-${variant}-800`]: variant,
   });
 
-  return <div className={`${badgeClasses}`}>{children}</div>;
+  return <div className={`w-10 h-2 ${badgeClasses}`}>{children}</div>;
 };
 
 export default Badge;
