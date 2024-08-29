b4e57eafba1ae2c4166c6b155579d832b3cbbb64
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-29 13:38:00 +0300

    add level 1 heading

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Badge from './Components/Badge';
 const App = () => {
   return (
     <main className='min-h-svh grid place-content-center'>
+      <h1>React Component Library</h1>
       <div className='flex gap-4 items-center'>
         <Badge shape='square' color='red'>
           Badge
