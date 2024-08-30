089840ff253705431f6da081d1e025917208ff0b
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-30 18:02:04 +0300

    add svg to testimonial icon prop

--- src/Components/Testimonial/TestimonialBlock.tsx
+++ src/Components/Testimonial/TestimonialBlock.tsx
@@ -4,7 +4,13 @@ const TestimonialBlock = () => {
   return (
     <div className='grid xxs:gap-28 sm:gap-14 md:gap-20 xl:gap-24'>
       <h2 className='h2'>Testimonial</h2>
-      <Testimonial accentColor='text-blue-700' name='May Anderson' company='Workcation' job='CTO'>
+      <Testimonial
+        accentColor='text-blue-700'
+        icon='./images/house-palm.svg'
+        name='May Anderson'
+        company='Workcation'
+        job='CTO'
+      >
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est
           integer dolor auctor adipiscing nunc urna, sit.
