b0076a5476148fa706b5ed60f25041f766ec3bf1
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-30 11:17:57 +0300

    create Card component structure

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { ReactElement, FC } from 'react';
 
-const Card = () => {
-  return <div>Card</div>;
+type CardProps = {
+  icon?: ReactElement;
+  children: React.ReactNode;
+  className?: string;
+  iconStyle?: string;
+};
+
+const Card: FC<CardProps> = ({ icon, className = '', iconStyle, children }) => {
+  return (
+    <div className={`card ${className}`}>
+      {icon && <div className={`card-icon ${iconStyle}`}>{icon}</div>}
+
+      <div className='grid gap-4'>{children}</div>
+    </div>
+  );
 };
 
 export default Card;
