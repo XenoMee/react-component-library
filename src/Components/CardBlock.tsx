361cc02e5dd7f53644170757509562aeda50afe4
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-30 11:30:17 +0300

    change background and scale hover effect for Card component

--- src/Components/CardBlock.tsx
+++ src/Components/CardBlock.tsx
@@ -5,7 +5,11 @@ const CardBlock = () => {
   return (
     <div className='grid xxs:gap-10 md:gap-16'>
       <h2 className='h2'>Card</h2>
-      <Card className='hover:bg-cyan-800' icon={<HiOutlineCloudUpload />} iconStyle='bg-blue-600 text-white'>
+      <Card
+        className='hover:bg-blue-200 hover:scale-110'
+        icon={<HiOutlineCloudUpload />}
+        iconStyle='bg-blue-600 text-white'
+      >
         <h3 className='font-bold text-[#111827]'>Easy Deployment</h3>
         <p>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
       </Card>
