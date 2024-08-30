e96b5a9b1461cc76c8016f1204a4ffd0a47f1721
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-30 11:24:58 +0300

    create CardBlock structure

---
+++ src/Components/CardBlock.tsx
@@ -0,0 +1,16 @@
+import Card from '../Components/Card';
+import { HiOutlineCloudUpload } from 'react-icons/hi';
+
+const CardBlock = () => {
+  return (
+    <div className='grid xxs:gap-10 md:gap-16'>
+      <h2 className='h2'>Card</h2>
+      <Card className='hover:bg-cyan-800' icon={<HiOutlineCloudUpload />} iconStyle='bg-blue-600 text-white'>
+        <h3 className='font-bold text-[#111827]'>Easy Deployment</h3>
+        <p>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
+      </Card>
+    </div>
+  );
+};
+
+export default CardBlock;
